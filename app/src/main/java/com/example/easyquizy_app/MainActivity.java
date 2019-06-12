9ef190694317e8eec561f790261aedd6c81b81b3
Inbal Schkolnik <capoeira0012>
2019-06-12 11:29:26 +0300

    MainActivity connected to LoginPage LoginPage changed to LoginPageActivity

--- app/src/main/java/com/example/easyquizy_app/MainActivity.java
+++ app/src/main/java/com/example/easyquizy_app/MainActivity.java
@@ -1,7 +1,10 @@
 package com.example.easyquizy_app;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,14 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button play = findViewById(R.id.play_btn);
+        play.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent homeIntent = new Intent(MainActivity.this, LoginPageActivity.class);
+                startActivity(homeIntent);
+            }
+        });
     }
 }
