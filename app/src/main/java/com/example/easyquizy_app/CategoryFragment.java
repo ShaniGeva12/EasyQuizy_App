7437d3f140754e250c1a91d8e7e080ad69af255b
DanielYadlin <dankayadlin@gmail.com>
2019-07-07 12:37:59 +0300

    StartGame + Activity Playing Added

--- app/src/main/java/com/example/easyquizy_app/CategoryFragment.java
+++ app/src/main/java/com/example/easyquizy_app/CategoryFragment.java
@@ -1,6 +1,7 @@
 package com.example.easyquizy_app;
 
 import android.content.Context;
+import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
@@ -13,6 +14,7 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Toast;
 
+import com.example.easyquizy_app.Common.Common;
 import com.example.easyquizy_app.Interface.ItemClickListener;
 import com.example.easyquizy_app.Model.Category;
 import com.example.easyquizy_app.ViewHolder.CategoryViewHolder;
@@ -103,7 +105,13 @@ public class CategoryFragment extends Fragment {
                 holder.setItemClickListener(new ItemClickListener() {
                     @Override
                     public void onClick(View view, int position, boolean isLongClick) {
-                        Toast.makeText(getActivity(),String.format("%s|%s",adapter.getRef(position).getKey(),model.getName()), Toast.LENGTH_SHORT).show();
+                        //Toast.makeText(getActivity(),String.format("%s|%s",adapter.getRef(position).getKey(),model.getName()), Toast.LENGTH_SHORT).show();
+                        Intent startGame = new Intent(getActivity(),Start.class);
+                        Common.categoryId = adapter.getRef(position).getKey();
+                        startActivity(startGame);
+
+
+
                     }
                 });
             }
---
+++ app/src/main/java/com/example/easyquizy_app/Common/Common.java
@@ -0,0 +1,17 @@
+package com.example.easyquizy_app.Common;
+
+import com.example.easyquizy_app.Model.Question;
+import com.example.easyquizy_app.Model.User;
+
+import java.util.ArrayList;
+import java.util.List;
+
+//global variables we need to use
+
+public class Common {
+
+    public static String categoryId;
+    public static User currentUser;
+    public static List<Question> questionList = new ArrayList<>();
+
+}
