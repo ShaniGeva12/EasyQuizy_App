119363e60adbcdfb721452aa81fa88c923ce2ba1
ShaniGeva12 <shanigeva666@gmail.com>
2019-07-07 19:11:34 +0300

    playing xml & java working (not complete)

--- app/src/main/java/com/example/easyquizy_app/Playing.java
+++ app/src/main/java/com/example/easyquizy_app/Playing.java
@@ -1,8 +1,9 @@
 package com.example.easyquizy_app;
 
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.SeekBar;
 import android.widget.Toast;
 
 public class Playing extends AppCompatActivity {
@@ -20,5 +21,32 @@ public class Playing extends AppCompatActivity {
         }
         else
             Toast.makeText(this, "play mode: " + playMode, Toast.LENGTH_SHORT).show();
+
+
+        Toast.makeText(this, "play mode: " + playMode, Toast.LENGTH_SHORT).show();
+
+
+        //seek bar - progress bar in the quiz
+        SeekBar seekBar = findViewById(R.id.seekBar);
+        if (seekBar != null) {
+            seekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+                @Override
+                public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                    // Write code to perform some action when progress is changed.
+                }
+
+                @Override
+                public void onStartTrackingTouch(SeekBar seekBar) {
+                    // Write code to perform some action when touch is started.
+                }
+
+                @Override
+                public void onStopTrackingTouch(SeekBar seekBar) {
+                    // Write code to perform some action when touch is stopped.
+                    Toast.makeText(Playing.this, "Current value is " + seekBar.getProgress(), Toast.LENGTH_SHORT).show();
+                }
+            });
+        }
+
     }
 }
