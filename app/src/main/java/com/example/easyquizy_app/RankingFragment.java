aaf5b4071e59337cf1475a42ed86efd8b3119421
ShaniGeva12 <shanigeva666@gmail.com>
2019-07-08 18:02:59 +0300

    Ranking = Score Board XML added

--- app/src/main/java/com/example/easyquizy_app/RankingFragment.java
+++ app/src/main/java/com/example/easyquizy_app/RankingFragment.java
@@ -1,18 +1,38 @@
 package com.example.easyquizy_app;
 
-import android.content.Context;
-import android.net.Uri;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.example.easyquizy_app.Model.Category;
+import com.example.easyquizy_app.ViewHolder.CategoryViewHolder;
+import com.firebase.ui.database.FirebaseRecyclerAdapter;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
 
 public class RankingFragment extends Fragment {
     View myFragment;
+    /*
+    //regular RecyclerView items
+    private RecyclerView recyclerView;
+    private RecyclerView.Adapter mAdapter;
+    private RecyclerView.LayoutManager layoutManager;
+    */
+
+    //fire base RecyclerView items
+    RecyclerView listCategory;
+    RecyclerView.LayoutManager layoutManager;
+    FirebaseRecyclerAdapter<Category, CategoryViewHolder> adapter;
+
+    FirebaseDatabase database;
+    DatabaseReference categories;
+
 
     public static RankingFragment newInstance(){
         RankingFragment rankingFragment = new RankingFragment();
@@ -22,12 +42,16 @@ public class RankingFragment extends Fragment {
     @Override
     public void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        database = FirebaseDatabase.getInstance();
+        categories = database.getReference("Category");
     }
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        myFragment = inflater.inflate(R.layout.fragment_ranking,container,false);
+        myFragment = inflater.inflate(R.layout.fragment_category,container,false);
         return myFragment;
     }
+
 }
